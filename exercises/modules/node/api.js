59050da8e30b12cd6b0fb16e8394ca4a90824337
seiftahawy54 <seiftahawy54@gmail.com>
2021-03-19 00:32:33 +0200

    First Exercise

---
+++ exercises/modules/node/api.js
@@ -0,0 +1,18 @@
+// global.App = global.App || {}
+const data = require("./data");
+
+const getUserById = (id, cb) => {
+  setTimeout(() => {
+    const user = data.users.find((user) => user.id === id);
+    cb(user);
+  }, 150);
+};
+
+const getPostsForUser = (userId, cb) => {
+  setTimeout(() => {
+    const posts = data.posts.filter((post) => post.createdBy === userId);
+    cb(posts);
+  }, 150);
+};
+
+module.exports = { getPostsForUser, getUserById };
