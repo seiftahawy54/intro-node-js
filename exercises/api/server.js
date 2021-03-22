04fd82dd996b5d75d35dc67495ed2b35f4854752
seiftahawy54 <seiftahawy54@gmail.com>
2021-03-22 16:23:36 +0200

    Finished working exercise

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -8,19 +8,18 @@ const mime = require("mime");
  * this function is blocking, fix that
  * @param {String} name full file name of asset in asset folder
  */
-const findAsset = (name) => {
+const findAsset = async (name) => {
   const assetPath = path.join(__dirname, "assets", name);
-  console.log(name);
-  // return fs.readFileSync(assetPath, { encoding: "utf-8" }).toString();
-  return new Promise((resolve, reject) => {
-    fs.readFile(assetPath, { encoding: "utf-8" }, (error, result) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(result);
-      }
-    });
-  });
+  return await fs.readFileSync(assetPath, { encoding: "utf-8" }).toString();
+  // return new Promise((resolve, reject) => {
+  //   fs.readFile(assetPath, { encoding: "utf-8" }, (error, result) => {
+  //     if (error) {
+  //       reject(error);
+  //     } else {
+  //       resolve(result);
+  //     }
+  //   });
+  // });
 };
 
 const hostname = "127.0.0.1";
@@ -30,7 +29,7 @@ const router = {
     asset: "index.html",
     mime: mime.getType("html"),
   },
-  "/style GET": {
+  "/style.css GET": {
     asset: "style.css",
     mime: mime.getType("css"),
   },
@@ -44,12 +43,22 @@ const server = http.createServer(async (req, res) => {
   const method = req.method;
   const route = url.parse(req.url).pathname;
   const match = router[`${route} ${method}`];
+  // if (!match) {
+  //   res.writeHead(404);
+  //   logRequest(method, route, 404);
+  //   return res.end();
+  // }
+  // res.writeHead(200, { "Content-Type": match.mime });
+  // logRequest(method, route, 200);
+  // res.write(await findAsset(match.asset));
+  // res.end();
+
   if (!match) {
     res.writeHead(404);
     logRequest(method, route, 404);
-    res.end();
-    return;
+    return res.end();
   }
+
   res.writeHead(200, { "Content-Type": match.mime });
   logRequest(method, route, 200);
   res.write(await findAsset(match.asset));
